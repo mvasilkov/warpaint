a9e269be99f2321b87fa0d0c06e155f090435439
Mark Vasilkov <mvasilkov@gmail.com>
2014-05-27 21:15:42 +0300

    cat

--- warpaint.js
+++ warpaint.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
+(function () {
+    function warpaint(s) {
+        s = s.trim()
+        return s
+    }
 
-function cli() {
-}
+    function cli() {
+        var fs = require('fs'),
+            s = fs.readFileSync(process.argv[2], {encoding: 'utf8'})
+        console.log(warpaint(s))
+    }
 
-if (typeof require === 'function' && typeof module === 'object' &&
-    require.main === module) cli()
+    if (typeof require == 'function' && typeof module == 'object') {
+        if (require.main === module) cli()
+        else if (module.exports) module.exports = warpaint
+    }
+    else if (typeof define == 'function' && define.amd)
+        define(function () { return warpaint })
+    else if (typeof window == 'object') window.warpaint = warpaint
+}())
--- warpaint.py
+++ warpaint.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+
+def warpaint(s):
+    s = s.strip()
+    return s
+
+
 def cli():
-    pass
+    import sys
+    s = open(sys.argv[1], encoding='utf8').read()
+    print(warpaint(s))
 
 if __name__ == '__main__':
     cli()
