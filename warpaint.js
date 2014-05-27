3cac0c9345f85f7a440b4a13f06ce8fb8215b012
Mark Vasilkov <mvasilkov@gmail.com>
2014-05-27 20:30:11 +0300

    Groundwork

---
+++ warpaint.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+function cli() {
+}
+
+if (typeof require === 'function' && typeof module === 'object' &&
+    require.main === module) cli()
---
+++ warpaint.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def cli():
+    pass
+
+if __name__ == '__main__':
+    cli()
